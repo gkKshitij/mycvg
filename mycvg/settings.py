b202d6209fa1e1a321449a3fe372fa3d81af3417
gkKshitij <42409384+gkKshitij@users.noreply.github.com>
2021-08-14 21:22:30 +0530

    changed django secret key

--- mycvg/settings.py
+++ mycvg/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8ip_=szu)ljvc8sv_y-h_x1w3n^v71(r!5ub_g$pn#oy*s*-03'
+SECRET_KEY = 'django-insecure-+ysx+e2_#z)f-g!8h#^k(6+v089@3#9=isv)!co3_hfwau8tyl'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
