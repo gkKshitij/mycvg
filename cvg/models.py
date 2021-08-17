d97d89e6dfa47fff3fb195879626128ad04a6ed6
Kshitij K <gk.kshitij@gmail.com>
2021-08-17 14:36:18 +0530

    broken login

--- cvg/models.py
+++ cvg/models.py
@@ -9,6 +9,7 @@ class Cv(models.Model):
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
+    address = models.TextField()
 
     def published(self):
         self.published_date = timezone.now()
--- mycvg/settings.py
+++ mycvg/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8ip_=szu)ljvc8sv_y-h_x1w3n^v71(r!5ub_g$pn#oy*s*-03'
+SECRET_KEY = 'django-insecure-8ip_=szu)ljvc8sv_y-h_x1w3n^v71(r!5ub_g$pn#oy*s*-03' #'django-insecure-vs_wtrki!^i3v*njd&bb!)@_0jmunus-n-av6gs#=)t@^e53@c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
